2b0d74f345b018bc04e0b85e264d858071095c8b
Harun Huseinspahic <harunhuseinspahic2001@gmail.com>
2024-11-17 22:35:05 +0100

    Added photos to public folder

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -9,6 +9,14 @@ const Hero = () => {
             <h1>GET</h1>
             <h1>MOVING</h1>
         </div>
+        <div className="sub-title">
+            <p>Your Journey to Fitenss Starts Here</p>
+            <p>Unleash Your Potential</p>
+        </div>
+        <div className="buttons">
+            <button>Start Your Journey</button>
+            <button>Discover Your Plan</button>
+        </div>
       </div>
     </section>
   )
